f5a9ffd92fc436b6503da3d038e07a5d8da8cc2e
tracer-ics2018 <tracer@njuics.org>
2018-11-06 12:11:29 +0000

    >  compile 007 m Linux 0a27511a2fc2 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  12:11:29 up 4 days,  2:31, 10 users,  load average: 0.00, 0.00, 0.00 4da4866e76dea47fd17d4dc570ec40c81bab07ef

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -23,7 +23,8 @@ static struct rule {
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-
+  {"(", '('},
+  {")", ')'},
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
