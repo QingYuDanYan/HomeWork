b157007d52eadf2722db3c38b44914a09a8a2f86
tracer-ics2018 <tracer@njuics.org>
2018-11-07 04:54:05 +0000

    >  compile 007 m Linux 0a27511a2fc2 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  04:54:05 up  1:56,  9 users,  load average: 0.08, 0.05, 0.02 f74af460b302d957404b10742219f18f7a2aba5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -60,7 +60,7 @@ static struct {
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 static int cmd_p(char *args) {
-  char *arg = strtok(NULL, " ");
+  char *arg = strtok(NULL, "");
   bool *success = false;
   expr(arg, success);
 
