b4e115045c8e8e8c8c7403b4488a677daf16ed92
tracer-ics2018 <tracer@njuics.org>
2018-11-09 04:21:10 +0000

    >  compile 007 m Linux 0a27511a2fc2 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  04:21:10 up 2 days,  1:23, 10 users,  load average: 0.01, 0.03, 0.00 9b06ef86b78c463fae745d2f40f08e58600ddeb

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monitor/expr.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
+  if (argc == 2 && strcmp(argv[1], "expr_test") == 0) {
+    printf("expr_test begin\n");
+    return 0;
+  }  
+
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
