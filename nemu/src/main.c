8781ef31da4c260db13d298c7b1f56a3e2adf2d7
tracer-ics2018 <tracer@njuics.org>
2018-11-09 04:39:40 +0000

    >  compile 007 m Linux 0a27511a2fc2 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  04:39:40 up 2 days,  1:42,  9 users,  load average: 0.00, 0.00, 0.00 dbf1d1895117e479e33c420d181f496927240cdf

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -8,6 +8,20 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   if (argc == 2 && strcmp(argv[1], "expr_test") == 0) {
     printf("expr_test begin\n");
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    fp = fopen("../tools/gen-expr/expr_testcase", "r");
+    if (fp == NULL) Assert(0, "expr_testcase is NULL\n");
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+      printf("%s", line);
+    }
+
+    free(line);
+    fclose(fp);
+
     return 0;
   }  
 
