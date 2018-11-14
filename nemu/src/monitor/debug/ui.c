082b753e4be48b0895299baa18ebaba9a1a64749
tracer-ics2018 <tracer@njuics.org>
2018-11-14 09:52:10 +0000

    >  compile 007 m Linux 0a27511a2fc2 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  09:52:10 up 22:35, 11 users,  load average: 0.08, 0.04, 0.00 80c95439f4d70e2e8deda897b6084b1bb7a0a10a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -156,9 +156,9 @@ static int cmd_info_w() {
   WP *head = get_head();
   char *header_format = "%-*s%-*s%*s\n";
   char *format = "%-*d%-*s%*s\n";
-  printf(header_format, 10, "Num", 15, "Type", 15, "What");
+  printf(header_format, 8, "Num", 15, "Type", 15, "What");
   while (head != NULL) {
-    printf(format, 10, head->NO, 15, "watchpoint", 15, head->expr);
+    printf(format, 8, head->NO, 15, "watchpoint", 15, head->expr);
     head = head->next;
   }
 
